f73a152932e522efe09f89ec1519cecb42c961f4
MatsVestvik <mats.o.vestvik@gmail.com>
2026-01-09 13:51:02 +0100

    root swap

--- src/main/java/com/stocktcg/Main.java
+++ src/main/java/com/stocktcg/Main.java
@@ -1,15 +1,19 @@
 package com.stocktcg;
 
+
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+
+import com.stocktcg.roots.TitleRoot;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 public class Main extends Application {
     
     @Override
     public void start(Stage stage) {
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 800, 600);
+        TitleRoot titleRoot = new TitleRoot();
+        Scene scene = new Scene(titleRoot.getParent(),800,600);
         stage.setScene(scene);
         stage.setTitle("Stock TCG");
         stage.show(); 
